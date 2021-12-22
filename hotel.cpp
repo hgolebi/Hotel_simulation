4e24a02f7eb4124c8b7300e54168e173f4e12e42
wgrunwal <01159582@pw.edu.pl>
2021-12-22 16:28:45 +0100

    some semi-major fixes

--- hotel.cpp
+++ hotel.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <vector>
 #include "hotel.h"
-#include "room.h"
 
-Hotel(std::string name, std::vector<*Room> rooms, std::vector<*Worker> workers, std::vector<*Guests> guests, double capital);
+Hotel::Hotel(std::string name, std::vector<Room*> rooms, std::vector<Worker*> workers, std::vector<GroupOfGuests*> guests, double capital);
 {
     this-> name = name;
     this-> rooms = rooms;
--- room.cpp
+++ room.cpp
@@ -2,12 +2,12 @@
 #include <vector>
 #include "room.h"
 
-Room::Room(enum rank, int number_of_beds, double area, double fee, bool additional_furniture, Guests guests);
+Room::Room(enum rank, int number_of_beds, double area, double fee, bool additional_furniture, GroupOfGuests guests);
 {
     this-> rank = rank;
     this-> number_of_beds = number_of_beds;
     this-> area = area;
     this-> fee = fee;
     this-> additional_furniture = additional_furniture;
-    this-> Guests = guests;
+    this-> guests = guests;
 }
