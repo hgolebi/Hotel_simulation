1218d7fac2ba3169160272973cf6db88f2b2288c
wgrunwal <01159582@pw.edu.pl>
2022-01-26 18:22:46 +0100

    changed workers to map, added Info prototype

--- hotel.cpp
+++ hotel.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <random>
 #include <chrono>
 #include "hotel.h"
@@ -12,14 +13,40 @@ Hotel::Hotel(string name, int capital)
     this-> capital = capital;
 }
 
-void Hotel::Info()
+string Hotel::get_name()
 {
-    return;
+    return name;
 }
 
 int Hotel::get_capital()
 {
     return capital;
+}
+
+void Hotel::Info()
+{
+    cout << "LISTA GOŚCI: " << endl;
+    for (int i = 0; i < guests.size(); i++) {
+        cout << *(guests[i]);
+     }
+    cout << endl;
+
+    // Test Rooms from File
+    // for (int i = 0; i < num; i++) {
+    //    cout << "Type: " << h.rooms[i]->get_type() << " "
+    //    << "Number of beds: " << h.rooms[i]->get_number_of_beds() << " "
+    //    << "Area: " << h.rooms[i]->get_area() << " "
+    //    << "Fee: " << h.rooms[i]->get_fee() << " "
+    //    << "Additional furniture: " << h.rooms[i]->get_additional_furniture() << "\n";
+    // }
+
+    // map<string, vector<Worker*>>::iterator it;
+    // Test Workers from File
+    // for (it=h.workers.begin(); it!=h.workers.end(); ++it) {
+    //    for (int i=0; i<it->second.size(); i++)
+    //        cout << it->first << " " << "Id: " << it->second[i]->get_id() << " "
+    //        << "Cash: " << it->second[i]->get_cash() << endl;
+    // }
 }
 
 void Hotel::Add_Guests(int id, int size, int acc_length, int cash)
@@ -28,9 +55,9 @@ void Hotel::Add_Guests(int id, int size, int acc_length, int cash)
     guests.push_back(g);
 }
 
-void Hotel::Add_Room(std::string type, int number_of_beds, double area, double fee, bool additional_furniture)
+void Hotel::Add_Room(int id, std::string type, int number_of_beds, double area, double fee, bool additional_furniture)
 {
-    Room *r = new Room(type, number_of_beds, area, fee, additional_furniture);
+    Room *r = new Room(id, type, number_of_beds, area, fee, additional_furniture);
     rooms.push_back(r);
 }
 
@@ -39,133 +66,37 @@ void Hotel::Add_Worker(string type_of_worker, int id, int cash)
     if (type_of_worker == "croupier")
     {
         Croupier *worker = new Croupier(id, cash);
-        workers.push_back(worker);
+        workers["Croupier"].push_back(worker);
     }
     else if (type_of_worker == "receptionist")
     {
         Receptionist *worker = new Receptionist(id, cash);
-        workers.push_back(worker);
+        workers["Receptionist"].push_back(worker);
     }
     else if (type_of_worker == "waiter")
     {
         Waiter *worker = new Waiter(id, cash);
-        workers.push_back(worker);
+        workers["Waiter"].push_back(worker);
     }
     else if (type_of_worker == "room_service")
     {
         RoomService *worker = new RoomService(id, cash);
-        workers.push_back(worker);
+        workers["Room_service"].push_back(worker);
     }
-}
-
-void Hotel::Read_Guests_From_File(std::string FileName)
-{
-    ifstream fileIn;
-
-    int id;
-    int size;
-    int accomodation_length;
-    int cash;
-
-    fileIn.open(FileName.c_str());
-    if (!fileIn.is_open()) {
-        cout << "Can not read file\n";
-    }
-
-    string line;
-    getline(fileIn, line);
-
-    while (fileIn >> id >> size >> accomodation_length >> cash) {
-        Add_Guests(id, size, accomodation_length, cash);
-    }
-
-    if ( fileIn.is_open() ) {
-        fileIn.close();
-    }
-
-    // Test Guests from File
-    for (int i = 0; i < guests.size(); i++) {
-        cout << "Id: " << guests[i]->get_id() << " "
-        << "Size: " << guests[i]->get_size() << " "
-        << "Accomodation length: " << guests[i]->get_acc_length() << " "
-        << "Cash: " << guests[i]->get_cash() << "\n";
-    }
-
-}
-
-void Hotel::Read_Rooms_From_File(std::string FileName)
-{
-    ifstream fileIn;
-
-    string type;
-    int number_of_beds;
-    double area;
-    double fee;
-    bool additional_furniture;
-
-    fileIn.open(FileName.c_str());
-    if (!fileIn.is_open()) {
-        cout << "Can not read file\n";
-    }
-
-    string line;
-    getline(fileIn, line);
-
-    while (fileIn >> type >> number_of_beds >> area >> fee >> additional_furniture) {
-        Add_Room(type, number_of_beds, area, fee, additional_furniture);
-    }
-
-    if ( fileIn.is_open() ) {
-        fileIn.close();
-    }
-
-    // Test Rooms from File
-    for (int i = 0; i < rooms.size(); i++) {
-        cout << "Type: " << rooms[i]->get_type() << " "
-        << "Number of beds: " << rooms[i]->get_number_of_beds() << " "
-        << "Area: " << rooms[i]->get_area() << " "
-        << "Fee: " << rooms[i]->get_fee() << " "
-        << "Additional furniture: " << rooms[i]->get_additional_furniture() << "\n";
-    }
-}
-
-void Hotel::Read_Workers_From_File(std::string FileName)
-{
-    ifstream fileIn;
-
-    string type_of_worker;
-    int id;
-    int cash;
-
-    fileIn.open(FileName.c_str());
-    if (!fileIn.is_open()) {
-        cout << "Can not read file\n";
-    }
-
-    string line;
-    getline(fileIn, line);
-
-    while (fileIn >> type_of_worker >> id >> cash) {
-        Add_Worker(type_of_worker, id, cash);
-    }
-
-    if ( fileIn.is_open() ) {
-        fileIn.close();
-    }
-
-    // Test Workers from File
-    for (int i = 0; i < rooms.size(); i++) {
-        cout << "id: " << workers[i]->get_id() << " " // tutaj by sie przydalo wypisywac jakos typ workera
-        << "Cash: " << workers[i]->get_cash() << "\n";
-    }
-}
-
-void Hotel::Simulate()
-{
-    return;
 }
 
 Hotel::~Hotel()
+{
+    for (auto guest : guests)
+        delete guest;
+    for (auto room : rooms)
+        delete room;
+    map<string, vector<Worker*>>::iterator it;
+    for (it=workers.begin(); it!=workers.end(); ++it)
+        delete &(it->second);
+}
+
+void Hotel::Simulate()
 {
     return;
 }
--- hotel.h
+++ hotel.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 #include "room.h"
 #include "worker.h"
 #include "group_of_guests.h"
@@ -21,21 +22,19 @@ class Hotel : public Simulation
         string name;
         vector<GroupOfGuests*> guests;
         vector<Room*> rooms;
-        vector<Worker*> workers;
+        map<string, vector<Worker*>> workers;
         int capital;
     public:
         Hotel(string, int = 10000000);
         void Info();
-        void Add_Room(string, int, double, double, bool);
+        void Add_Room(int, string, int, double, double, bool);
         void Add_Worker(string, int, int);
         void Add_Guests(int, int, int, int);
         ~Hotel();
 
+        string get_name();
         int get_capital();
         void Simulate() override;
-        void Read_Guests_From_File(string) override;
-        void Read_Rooms_From_File(string) override;
-        void Read_Workers_From_File(string) override;
 
         friend class Data;
 };
