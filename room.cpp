cb375b88a617af6d26290b4b65709bcc27e5ded3
wgrunwal <01159582@pw.edu.pl>
2022-01-26 18:20:50 +0100

    Added Room id, get_id and << operator

--- room.cpp
+++ room.cpp
@@ -2,18 +2,27 @@
 #include <vector>
 #include "room.h"
 
-Room::Room(std::string type, int number_of_beds, double area, double fee, bool additional_furniture)
+Room::Room(int id, std::string type, int number_of_beds, double area, double fee, bool additional_furniture)
 {
+    this-> id = id;
     this-> type = type;
     this-> number_of_beds = number_of_beds;
     this-> area = area;
     this-> fee = fee;
     this-> additional_furniture = additional_furniture;
+
+    GroupOfGuests g;
+    this-> guests = g;
 }
 
-void Room::change_guests(GroupOfGuests guests)
+void Room::change_guests(GroupOfGuests &guests)
 {
     this -> guests = guests;
+}
+
+int Room::get_id() const
+{
+    return id;
 }
 
 std::string Room::get_type() const
@@ -38,4 +47,19 @@ double Room::get_fee() const
 bool Room::get_additional_furniture() const
 {
     return additional_furniture;
+}
+
+ostream& operator<<(ostream& os, Room& r)
+{
+    if (r.guests != NULL)
+    {
+        os<<"Group no. "<<g.get_id()<<" consisting of "<<g.get_size()<<" people stays in room "<<g.get_room_id()<<"."<<endl;
+        os<<"Staying for: "<<g.get_acc_length()<<" day(s). Available cash: "<<g.get_cash()<<"$"<<endl;
+    }
+    else
+    {
+        os<<"Group no. "<<g.get_id()<<" consisting of "<<g.get_size()<<" people isn't staying in this hotel."<<endl;
+        os<<"Available cash: "<<g.get_cash()<<"$"<<endl;
+    }
+    return os;
 }
--- room.h
+++ room.h
@@ -8,6 +8,7 @@
 class Room
 {
     private:
+        int id;
         std::string type;
         int number_of_beds;
         double area;
@@ -15,13 +16,18 @@ class Room
         bool additional_furniture;
         GroupOfGuests guests;
     public:
-        Room(std::string, int, double, double, bool);
-        void change_guests(GroupOfGuests guests);
+        Room(int, std::string, int, double, double, bool);
+        void change_guests(GroupOfGuests &guests);
+        int get_id() const;
         std::string get_type() const;
         int get_number_of_beds() const;
         double get_area() const;
         double get_fee() const;
         bool get_additional_furniture() const;
+
+        friend ostream& operator<<(ostream& os, Room& r);
 };
+
+ostream& operator<<(ostream& os, Room& r);
 
 #endif
